4f96974ca1f4b744e69d667cdc15ecd4ea95f6d7
maria-montaha <mariamontaha2023@gmail.com>
2024-07-09 21:35:30 +0600

    environmentsetup,data and attribut binding , add git link

---
+++ part3/app.js
@@ -0,0 +1,13 @@
+var app = Vue.createApp({
+    data() {
+        return {
+            msg: "Welcome to Vue js 3",
+        };
+    },
+});
+
+app.mount("#app");
+
+
+
+
---
+++ part5/app.js
@@ -0,0 +1,15 @@
+var app = Vue.createApp({
+    data() {
+        return {
+            msg: "Welcome to Vue js 3",
+            info: "Hello from Vue js 3",
+            github: "https://github.com/Maria-Montaha",
+        };
+    },
+});
+
+app.mount("#app");
+
+
+
+
